1fd9b7e00e621c640a8f4e6c1126ac6cd08baae4
arief2209 <arief.rachman2209@gmail.com>
2024-05-28 01:13:53 +0700

    Artisan Command to Create Users

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,8 +48,8 @@ class User extends Authenticatable
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
      */
-    public function roles(): BelongsToMany
+    public function roles()
     {
-        return $this->belongsToMany(Role::class, 'role_user_table', 'user_id', 'role_id');
+        return $this->belongsToMany(Role::class, 'role_user', 'user_id', 'role_id');
     }
 }
